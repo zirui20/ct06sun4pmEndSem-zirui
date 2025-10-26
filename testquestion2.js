cd553438d167b9492eb936e96ed315b0e7402ed0
zirui20 <firefishpro@gmail.com>
2025-10-26 09:12:29 +0000

    Oct 26, 2025, 9:12 AM

--- testquestion2.js
+++ testquestion2.js
@@ -4,14 +4,14 @@ function setup() {
     createCanvas(200,300);
 }
 function draw() {
-    background("black");
+    background("white");
 
     fill('pink');
     textAllign(CENTER,CENTER);
     textSize(20);
 
     for(let i=6; i<pokemons.length; i++ ) {
-            
+        print(text)
     }
 
 }
