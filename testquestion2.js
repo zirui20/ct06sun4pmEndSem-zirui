dc305aa8f2bd20caaab7a3da029aaf5c9bad765d
zirui20 <firefishpro@gmail.com>
2025-10-26 09:03:07 +0000

    Oct 26, 2025, 9:03 AM

--- testquestion2.js
+++ testquestion2.js
@@ -9,4 +9,8 @@ function draw() {
     fill('pink');
     textAllign(CENTER,CENTER);
     textSize(20);
+
+    for (let i=0; i<arr.length; i++) {
+        text(arr[i], width/2, 100 + i*50);
+    }
 }
