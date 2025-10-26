95d1c9379f936dc562839ca20ad59a31f7087608
zirui20 <firefishpro@gmail.com>
2025-10-26 08:52:40 +0000

    26/10

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,22 @@
+let x = 300;
+let xSpeed = 5;
+let radius = 25;
 
+
+function setup() {
+    createCanvas(600,400);
+}
+function draw() {
+    background('lightblue');
+    noStroke();
+    fill('limegreen');
+    circle(x,200,50);
+
+    if(x>=width-radius || x<=radius) {
+        xSpeed *=-1;
+    }
+
+
+    x += xSpeed;
+    circle(x,200,50);
+}
